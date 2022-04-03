b62798cbb7db43c0e681f8df37b3b2649ba08d2a
Morris Brodersen <mb@morrisbrodersen.de>
2022-04-03 10:20:54 +0200

    add node 16 pipeline

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,6 @@
 set -ex
 
-rm -rf .nyc_output build coverage dist
+rm -rf build coverage dist
 rm -rf examples/client/public/*.js
 rm -rf examples/client/public/*.js.map
 rm -rf examples/client/node_modules
