463e04f88ffef53808cfb03ed26b3b7fda29162d
Morris Brodersen <mb@morrisbrodersen.de>
2022-06-13 23:13:21 +0200

    export GraphQLRequestError

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,5 +1,6 @@
 /// <amd-module name='graphqlade/dist/browser'/>
 export * from "../client/GraphQLClient";
+export * from "../client/GraphQLRequestError";
 export * from "../util/AsyncPushIterator";
 export * from "../ws/GraphQLClientWebSocket";
 export * from "../ws/GraphQLWebSocketClient";
